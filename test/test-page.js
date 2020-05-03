98e881fb2583fbce138f8dec093a2b3c1a0cdca6
root <thiago.csilva94@gmail.com>
2020-05-03 16:39:01 +0000

    test stage 3

--- test/test-page.js
+++ test/test-page.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
+    request('http://simpleweb-env.eba-kxpjg2sn.us-east-2.elasticbeanstalk.com' , function(error, response, body) {
         expect(body).to.equal('ola mundo');
         done();
     });
