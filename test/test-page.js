497590befe12f9422a0cdcc2ae301e8ec3c032d1
root <thiago.csilva94@gmail.com>
2020-05-07 16:57:05 +0000

    test added

--- test/test-page.js
+++ test/test-page.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://simpleweb-env.eba-kxpjg2sn.us-east-2.elasticbeanstalk.com' , function(error, response, body) {
+    request('http://simpleweb-env.eba-scdw2yvd.us-east-2.elasticbeanstalk.com/' , function(error, response, body) {
         expect(body).to.equal('ola mundo');
         done();
     });
